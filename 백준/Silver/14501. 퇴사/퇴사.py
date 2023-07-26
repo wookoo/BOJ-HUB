3c1241d1a58d2b3a8fd2e1670d442a3e2505f4c3
김도현 <39308953+wookoo@users.noreply.github.com>
2023-07-26 20:52:20 +0900

    [Silver III] Title: 퇴사, Time: 132 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,26 @@
+import sys
+#sys.stdin = open("input.txt")
+size = int(input())
+N = [0]*size
+P = [0]*size
+cost = [0]*(size+1)
+for i in range(size):
+    t,j = map(int,input().split())
+    N[i] = t
+    P[i] = j
+
+for i in range(size):
+    endTime= N[i]
+    endCost= P[i]
+
+
+for i in range(size):
+    endTime = N[i]
+    endCost = P[i]
+    for j in range(i+endTime,size+1):
+        cost[j] = max(cost[j],endCost+cost[i])
+
+
+
+
+print(cost[-1])
