106e4297dac8642589b8e94fb52f3639153f1a68
김도현 <39308953+wookoo@users.noreply.github.com>
2023-08-02 01:00:07 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception{
+		
+			System.out.println("Hello World!");
+			
+		
+	}
+	
+}
