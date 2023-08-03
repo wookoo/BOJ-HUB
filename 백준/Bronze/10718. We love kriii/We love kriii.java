9e353a7fa870c524e268dedbf4cd56c1169d918c
김도현 <39308953+wookoo@users.noreply.github.com>
2023-08-04 00:48:53 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//int SIZE = Integer.parseInt(br.readLine());
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		bw.write("\uAC15\uD55C\uCE5C\uAD6C \uB300\uD55C\uC721\uAD70");
+		bw.newLine();
+		bw.write("\uAC15\uD55C\uCE5C\uAD6C \uB300\uD55C\uC721\uAD70");
+		bw.newLine();
+		
+
+		bw.flush();
+		
+
+	}
+
+
+}
