4548358d612cefcd1d8aca635903c43e8469f948
김도현 <39308953+wookoo@users.noreply.github.com>
2023-08-11 11:04:59 +0900

    [Bronze IV] Title: 베라의 패션, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+/**
+ * @author ssafy
+ */
+public class Main {
+
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int size = Integer.parseInt(br.readLine());
+		System.out.println(size*(size-1));
+		
+	
+		
+	}
+	
+	
+
+}
+
