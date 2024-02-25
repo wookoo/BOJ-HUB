ff2ae2bc80556f2b1f2cb5387726e440459951e8
김도현 <39308953+wookoo@users.noreply.github.com>
2024-02-26 00:12:56 +0900

    [Gold II] Title: 트리의 지름, Time: 792 ms, Memory: 104072 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+
+    static int ans = 0;
+    static boolean visited[];
+    static ArrayList<ArrayList<int[]>> nodes;
+    static int maxS = 0;
+
+    public static void main(String[] args) throws Exception {
+
+        //시작점은 단말노드로부터 시작 -> 노드가 한개인것만.
+
+        //System.setIn(new FileInputStream("src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int nodeCnt = Integer.parseInt(br.readLine());
+        nodes = new ArrayList<ArrayList<int[]>>();
+        visited = new boolean[nodeCnt];
+        for (int i = 0; i < nodeCnt; i++) {
+            nodes.add(new ArrayList<int[]>());
+        }
+        for (int i = 0; i < nodeCnt; i++) {
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(stk.nextToken()) - 1;
+            int toward = 0;
+            ArrayList<int[]> addNode = nodes.get(start);
+            while (true) {
+                toward = Integer.parseInt(stk.nextToken()) - 1;
+                if (toward <= -1) {
+                    break;
+                }
+                int cost = Integer.parseInt(stk.nextToken());
+                addNode.add(new int[]{toward, cost});
+            }
+        }
+        Arrays.fill(visited, false);
+        dfs(0,0);
+        ans = 0;
+        Arrays.fill(visited, false);
+        dfs(maxS,0);
+
+        System.out.println(ans);
+    }
+
+    public static void dfs(int start, int cost) {
+        visited[start] =true;
+        if(cost > ans){
+            ans = cost;
+            maxS = start;
+        }
+        ArrayList<int[]> arr = nodes.get(start);
+        for(int[] node: arr){
+            int s = node[0];
+            int c = node[1];
+            if(visited[s]){
+                continue;
+            }
+            dfs(s,c+cost);
+        }
+
+
+
+    }
+
+
+}
